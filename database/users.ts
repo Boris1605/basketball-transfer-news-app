d00e06432af7324f4e993291edba390a0c46ea9f
Boris Djuric <borisdjuric@Boris-MacBook-Pro.local>
2024-02-29 17:36:39 +0100

    Add users  migrations

--- database/users.ts
+++ database/users.ts
@@ -1,5 +1,5 @@
-import { cache } from 'react';
-import { sql } from './connect';
+// import { cache } from 'react';
+// import { sql } from './connect';
 
 export type User = {
   id: number;
@@ -10,47 +10,47 @@ export type UserWithPasswordHash = User & {
   passwordHash: string;
 };
 
-export const getUserByEmailInsecure = cache(async (email: string) => {
-  const [user] = await sql<User[]>`
-    SELECT
-      id,
-      email
-    FROM
-      users
-    WHERE
-      email = ${email.toLowerCase()}
-  `;
-  return user;
-});
+// export const getUserByEmailInsecure = cache(async (email: string) => {
+//   const [user] = await sql<User[]>`
+//     SELECT
+//       id,
+//       email
+//     FROM
+//       users
+//     WHERE
+//       email = ${email.toLowerCase()}
+//   `;
+//   return user;
+// });
 
-export const getUserWithPasswordHashByEmailInsecure = cache(
-  async (email: string) => {
-    const [user] = await sql<UserWithPasswordHash[]>`
-      SELECT
-        *
-      FROM
-        users
-      WHERE
-        email = ${email.toLowerCase()}
-    `;
-    return user;
-  },
-);
+// export const getUserWithPasswordHashByEmailInsecure = cache(
+//   async (email: string) => {
+//     const [user] = await sql<UserWithPasswordHash[]>`
+//       SELECT
+//         *
+//       FROM
+//         users
+//       WHERE
+//         email = ${email.toLowerCase()}
+//     `;
+//     return user;
+//   },
+// );
 
-export const createUserInsecure = cache(
-  async (email: string, passwordHash: string) => {
-    const [user] = await sql<User[]>`
-      INSERT INTO
-        users (email, password_hash)
-      VALUES
-        (
-          ${email.toLowerCase()},
-          ${passwordHash}
-        )
-      RETURNING
-        id,
-        email
-    `;
-    return user;
-  },
-);
+// export const createUserInsecure = cache(
+//   async (email: string, passwordHash: string) => {
+//     const [user] = await sql<User[]>`
+//       INSERT INTO
+//         users (email, password_hash)
+//       VALUES
+//         (
+//           ${email.toLowerCase()},
+//           ${passwordHash}
+//         )
+//       RETURNING
+//         id,
+//         email
+//     `;
+//     return user;
+//   },
+// );
