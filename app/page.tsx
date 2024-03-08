bab5404a4e907089a8840a472b79d3321cab5fe1
Boris Djuric <boris_djuric@hotmail.com>
2024-03-08 12:52:31 +0100

    Change profile layout for testing and shorten homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,9 @@ export default function HomePage() {
               Welcome to Your Go-To website for Basketball News and Transfers!
               Stay up-to-date with all things basketball, from the latest news
               to player transfers. Whether you're a hardcore fan or just
-              curious, we've got you covered. Our team keeps you in the loop
-              with real-time updates on player movements and trade rumors, so
-              you never miss a beat. From the NBA to EuroLeague (coming soon) ,
-              we've got everything you need to stay in the game. Dive in and
-              explore the world of basketball news and transfers!
+              curious, we've got you covered. From the NBA to EuroLeague (coming
+              soon) , we've got everything you need to stay in the game. Dive in
+              and explore the world of basketball news and transfers!
             </p>
           </div>
         </div>
--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -2,14 +2,7 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { getUser } from '../../../database/users';
 
-type Props = {
-  params: {
-    id: string;
-    email: string;
-  };
-};
-
-export default async function UserProfilePage(props: Props) {
+export default async function UserProfilePage() {
   // 1. Check if the sessionToken cookie exists
   const sessionTokenCookie = cookies().get('sessionToken');
 
@@ -23,14 +16,18 @@ export default async function UserProfilePage(props: Props) {
 
   // 4. If user exists, render the page
   return (
-    <div>
-      <div className="avatar">
-        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-          {/* static avatar */}
-          <img src="../../images/avatar/avatar1.webp" alt="avatar" />
+    <main>
+      <div>
+        <h1>Your Profile</h1>
+        <br />
+        <div className="avatar">
+          <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            {/* static avatar */}
+            <img src="../../images/avatar/avatar1.webp" alt="avatar" />
+          </div>
         </div>
+        <p>Favorite Team's</p>
       </div>
-      <h2>{props.params.email}'s Profile</h2>
-    </div>
+    </main>
   );
 }
